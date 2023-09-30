a9849f8043b1f0aa691a54278bd2a4356246d2d4
andrespa612 <38359603+andrespa612@users.noreply.github.com>
2023-09-29 20:12:01 -0600

    Cambiar nombres de metodos

--- Laboratorio6/UnitTest1.cs
+++ Laboratorio6/UnitTest1.cs
@@ -5,28 +5,28 @@ namespace Laboratorio6
     public class UnitTest1
     {
         [Fact]
-        public void TestSuma()
+        public void Calculadora_Suma_Caso()
         {
             Operaciones.Calculadora calculadora = new Operaciones.Calculadora();
             Assert.Equal(3, calculadora.Suma(1, 2));
         }
 
         [Fact]
-        public void TestResta()
+        public void Calculadora_Resta_Caso()
         {
             Operaciones.Calculadora calculadora = new Operaciones.Calculadora();
             Assert.Equal(3, calculadora.Resta(5, 2));
         }
 
         [Fact]
-        public void TestMulti()
+        public void Calculadora_Multiplicacion_Caso()
         {
             Operaciones.Calculadora calculadora = new Operaciones.Calculadora();
             Assert.Equal(3, calculadora.Multiplicar(1, 3));
         }
 
         [Fact]
-        public void TestDivision()
+        public void Calculadora_Division_Caso()
         {
             Operaciones.Calculadora calculadora = new Operaciones.Calculadora();
             Assert.Equal(3, calculadora.Dividir(9, 3));
